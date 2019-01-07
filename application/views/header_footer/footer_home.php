afc4f2342c7f6dc804d26f09445a029d53889f12
wildanal2-Pc <wildanal2@gmail.com>
2019-01-07 13:43:58 +0700

    update home

--- application/views/header_footer/footer_home.php
+++ application/views/header_footer/footer_home.php
@@ -1,31 +1,23 @@
 
 		<nav class="navbar navbar-default navbar-fixed-bottom" style="background: #4f0381" role="navigation">
-			
-				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" style="background-color: #ffff00">
-					<h4 style="color: #ffffff" style="text-align: center;" id="time"></h4>
+			<div class="container-fluid">
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="background-color: #ff9900;">
+					<center><h4 style="color: #ffffff" id="time"></h4></center>
 				</div>
 				
-				<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+				<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
 					<div class="runtext-container">
-					<div class="main-runtext">
-					<marquee direction="" 
-					onmouseover="this.stop();"
-					onmouseout="this.start();">
-
-					
-
-					     <div class="text-container">
-					   &nbsp; &nbsp; &nbsp;<a data-fancybox-group="gallery" class="fancybox" href="#" style="color: #ffffff"><h5><img src="assets/image/logo.png" height="20px">DINAS KOMUNIKASI DAN INFORMATIKA</h5></a>
-					    </div>
-						
+						<div class="main-runtext">
+							<marquee direction="" onmouseover="this.stop();"onmouseout="this.start();">
+								<div class="text-container">
+								   <a data-fancybox-group="gallery" class="fancybox" href="#" style="color: #ffffff"><h5><img src="assets/image/logo.png" height="20px">DINAS KOMUNIKASI DAN INFORMATIKA</h5></a>
+								</div>
+							</marquee>
+						</div>
 					</div>
+				</div> 
 
-					</marquee>
-					</div>
-					</div>
-				</div>
-
-				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" style="background-color: #ff1a1a"  >
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="background-color: #ff1a1a"  >
 					<h4 style="color: #ffffff" style="align-items: center;"> 
 						<?php
 						date_default_timezone_set("Asia/Jakarta");
@@ -33,9 +25,7 @@
 						?>
 					</h4>
 				</div>
-				
-
-			
+			</div>	
 		</nav>
 		
 
@@ -48,7 +38,7 @@
 		
 		<script type="text/javascript">
 
-			function display_c(){
+		function display_c(){
 				var refresh=1000; // Refresh rate in milli seconds
 				mytime=setTimeout('display_ct()',refresh)
 				}
@@ -60,6 +50,49 @@
 					display_c();
 				}
 
+
+		$(document).ready(function(){
+
+			showAgenda(); //call function show all agenda
+          
+	        //function show all agenda
+	        function showAgenda(){
+	            $.ajax({
+	                type  : 'ajax',
+	                url   : '<?php echo base_url();?>index.php/Home/getAgenda',
+	                dataType : 'json',
+	                success : function(data){
+	                    var html = '';
+	                    var i;
+	                    for(i=0; i<data.length; i++){
+	                        a=i+1;
+	                        
+	                        // var myDateTime = moment(xx).format('DD-MMM-YYYY');
+	                        
+	                        var tgl_a = new Date(data[i].tanggal_awal);
+	                        var tgl_awal = tgl_a.getDate()+"/"+tgl_a.getMonth()+1+"/"+tgl_a.getYear();
+	                        var tgl_b = new Date(data[i].tanggal_akhir);
+	                        var tgl_ahir = tgl_b.getDate()+"/"+tgl_b.getMonth()+1+"/"+tgl_b.getYear();
+	                        html += '<tr>';
+	                        		
+	                        		if (data[i].level == 1) {
+	                        html +=			'<td style="text-align: center" bgcolor="#66C99B"><font color="#fff">'+a+'</font></td>';
+	                        		}else if (data[i].level == 2) {
+	                        html +=			'<td style="text-align: center" bgcolor="#FE851C"><font color="#fff">'+a+'</font></td>';
+	                        		}
+		                    html +=	
+		                            '<td>'+data[i].namaKegiatan+'</td>'+
+		                            '<td style="text-align: center;">'+tgl_awal+' s.d '+tgl_ahir+'</td>'+ 
+	                            '</tr>';
+	                    }
+	                    $('#tbl_agendakegiatan').html(html); 
+	                }
+	            });
+	        }
+
+
+
+		});
 		</script>
 
 
--- application/views/home.php
+++ application/views/home.php
@@ -2,14 +2,30 @@
 ?> 
 
 
+<div class="container-fluid" style="width: 100%">
+	<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 boddy">
+		<h4 class="namatitel">AGENDA KEGIATAN</h4>
+		<table>
+		  <tr style="background-color: #E8E8E8">
+		    <th style="width: 10%;">No</th>
+		    <th style="text-align: center; width: 40%;">Kegiatan</th>
+		    <th style="text-align: right; width: 40%">Tgl Mulai & Selesai</th>
+		  </tr>
+			
+			<tbody id="tbl_agendakegiatan">
+				
+			</tbody> 
+		</table>
+		
 
+	</div>
+	<div class="col-xs-5 col-sm-4 col-md-4 col-lg-4 boddy">
+		<h4 class="namatitel">KALENDER KEGIATAN</h4>
+	</div>
+	<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 boddy">
+		<h4 class="namatitel">PENGUMUMAN</h4>
+	</div>
 
-
-
-
-
-
-
-
+</div>
 
 <?php $this->load->view('header_footer/footer_home'); ?>
