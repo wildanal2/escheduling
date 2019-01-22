ff3257adda00a9dc39d253b7338ba6f00b249374
Faa_dli <mzfadlii@gmail.com>
2019-01-22 12:14:07 +0700

    tmbah lagi

--- application/views/login/header_login.php
+++ application/views/login/header_login.php
@@ -7,12 +7,13 @@
         <title>Admin E-scheduling</title>
 
         <!-- Bootstrap CSS --> 
-        <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/bootstrap.min.css"> 
+        <!-- <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">  -->
 
         <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/style_login.css">
         
-        <script src="<?php echo base_url() ?>assets/js/jquery-3.3.1.min.js"></script>
-        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
  
     </head>
 
--- application/views/login/login_view.php
+++ application/views/login/login_view.php
@@ -4,47 +4,42 @@
         echo '<script> alert("'.$error.'"); 
             </script>';
     } ?>
-
-    <div class="container">
-        
+    <div class="container">       
         <br><br><br>
         <center>
-            <img src="<?=base_url()?>assets/image/logo.png" class="img-responsive" height="150px">            
+            <img src="<?=base_url()?>assets/image/logo2_Mojokerto.png" class="" height="110px">
         </center>
         <br>
         <div class="row">
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 login" >
-            <form id="form_login">
-            <fieldset>
-
-            <?php echo validation_errors(); ?>
-                <br>
-                <p class="text-center">Hey! Let's go to work.</p>
-                <div class="form-horizontal fomlogin">
-                    <div class="input-group textinput">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input id="uname" type="text" class="form-control" name="uname" placeholder="username">
-                    </div>
-                    <div class="input-group textinput">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="paswd" type="Password" class="form-control" name="paswd" placeholder="Password">
-                    </div>
-
-                    <div id="responseDiv" class="alert text-center" style="margin-top:20px; display:none;">
-                      <button type="button" class="close" id="clearMsg"><span aria-hidden="true">&times;</span></button>
-                      <span id="message"></span>
-                    </div>
-
-                    <button type="button" class="btn btn-success textinput" style="width:100%;" id="btn_login">Sign In</button>
-                    <br>
-                </div>
-
-            </fieldset>
-            </form> 
-
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 " >
+                <div class="login">
+                    <form id="form_login">
+                        <fieldset>
+                        <?php echo validation_errors(); ?>
+                            <br>
+                            <p class="text-center">Hey! Let's go to work.</p>
+                            <div class="form-horizontal fomlogin">
+                                <div class="input-group textinput">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                    <input id="uname" type="text" class="form-control" name="uname" placeholder="username">
+                                </div>
+                                <div class="input-group textinput">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                    <input id="paswd" type="Password" class="form-control" name="paswd" placeholder="Password">
+                                </div>
+                                <div id="responseDiv" class="alert text-center" style="margin-top:20px; display:none;">
+                                  <button type="button" class="close" id="clearMsg"><span aria-hidden="true">&times;</span></button>
+                                  <span id="message"></span>
+                                </div>
+                                <button type="button" class="btn btn-success textinput" style="width:100%;" id="btn_login">Sign In</button>
+                                <br>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div><br>
+                <center><p> &copy; Copyright 2019</p></center>
+            </div>
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>
-        </div>
-
     </div>
