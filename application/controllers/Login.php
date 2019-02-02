dc5ead80c77c47e79afd010a8c5e7192bc7bc8f2
wildanal2-mac <wildanal2@gmail.com>
2019-02-02 20:04:50 +0700

    upgrade security
    - add login attempts

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -25,18 +25,79 @@ class Login extends CI_Controller {
 					'level' => $row->level
 				);	 
 			} 
-			$this->session->set_userdata('escheduling_logged',$sess_array );
+			// set sesion logined
+			$this->session->set_userdata('escheduling_logged',$sess_array); 
 			$output['level'] = 1;
 			$output['message'] = 'Prosess Masuk. Tunggu sebentar...';
+			// unset attempt login unset
+			$this->session->unset_userdata('escheduling_loginattempt');
 		}else { 
 			$output['error'] = true;
 			$output['message'] = 'Gagal masuk. User atau Password tidak terdaftar';
+
+			// mmembuat login attempt 
+			// cek attempt
+			if ($this->session->userdata('escheduling_loginattempt')) {
+				$session_data = $this->session->userdata('escheduling_loginattempt'); 
+				$sessattempt_array = array();
+				// cek jika salah pwd lebih 5 kali
+				if ($session_data['jumlah']>5) {
+					$p =($session_data['penalty']*2);
+					$sessattempt_array = array( 
+											'jumlah' => $session_data['jumlah']+1,
+											'penalty' => $p
+										);	
+
+					$output['message'] = 'User atau Password tidak terdaftar';
+					// memasukkan data session
+					$this->session->set_userdata('escheduling_loginattempt',$sessattempt_array );  
+				}else if ($session_data['jumlah']==5) {
+					$sessattempt_array = array( 
+											'jumlah' => $session_data['jumlah']+1,
+											'penalty' => 15
+										);
+					// memasukkan data session
+					$this->session->set_userdata('escheduling_loginattempt',$sessattempt_array ); 
+				}else{
+					$sessattempt_array = array( 
+											'jumlah' => $session_data['jumlah']+1,
+											'penalty' => 0
+										);
+					// memasukkan data session
+					$this->session->set_userdata('escheduling_loginattempt',$sessattempt_array ); 
+				}
+				
+				// untuk mengirim informasi attempt login
+				$output['jumlah'] = $session_data['jumlah'];
+				$output['penalty'] = $session_data['penalty'];
+			}else { 
+				$sessattempt_array = array(
+											'jumlah' => 1,
+											'penalty' => 0
+										);  
+				// memasukkan data session
+				$this->session->set_userdata('escheduling_loginattempt',$sessattempt_array );
+			}
+
 		}
 
 		echo json_encode($output); 
 	}
  
-	
+	// fungsi untuk mengecek upaya login
+	public function cekattempt()
+	{
+		if ($this->session->userdata('escheduling_loginattempt')) {
+			$session_data = $this->session->userdata('escheduling_loginattempt'); 
+			
+			// untuk mengirim informasi attempt login
+			$output['jumlah'] = $session_data['jumlah'];
+			$output['penalty'] = $session_data['penalty'];
+		}else{
+			$output['jumlah'] = 0;
+		}
+		echo json_encode($output); 
+	}
 
 
 	public function logout()
--- application/views/login/footer_login.php
+++ application/views/login/footer_login.php
@@ -11,8 +11,54 @@
 
 		
 		<script type="text/javascript">
+		
+		function startTimer(duration, display) {
+		    var timer = duration, minutes, seconds;
+		    timee = setInterval(function () {
+		        minutes = parseInt(timer / 60, 10)
+		        seconds = parseInt(timer % 60, 10);
+
+		        minutes = minutes < 10 ? "0" + minutes : minutes;
+		        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+		        display.text(minutes + ":" + seconds);
+
+		        if (--timer < 0) {
+		            // $('#responseDiv').removeClass('alert-danger').hide();
+					$('#uname').val("");
+					$('#paswd').val("");
+		            document.getElementById("btn_login").disabled = false;
+		            display.text("Sign In");
+		            clearInterval(timee);
+		        }
+
+		    }, 1000);
+		}
 
 		$(document).ready(function(){
+
+			cekattempts();
+			function cekattempts(){
+				$.ajax({
+	            	async : false,
+	                type : "ajax",
+	                url   : '<?php echo base_url();?>index.php/Login/cekattempt',
+	                dataType : "JSON", 
+	                success: function(response){
+	                	
+						if (response.jumlah>5) {
+							$('#message').html("Terlalu banyak upaya login");
+							$('#responseDiv').removeClass('alert-success').addClass('alert-danger').show(); 
+
+							var secondd = response.penalty;
+						    display = $('#btn_login');
+						    startTimer(secondd, display); 
+							document.getElementById("btn_login").disabled = true;
+	                	}
+
+	                }
+	            });
+			}
  
 			$('#form_login').submit(function(e){
 	            var username = $('#uname').val();
@@ -24,14 +70,29 @@
 	                url   : '<?php echo base_url();?>index.php/Login/ceklogin',
 	                dataType : "JSON",
 	                data : {username:username,password:password},
-	                success: function(response){ 
-	                	$('#message').html(response.message);
-
+	                success: function(response){
+	                	
 						if(response.error){
+							if (response.jumlah>5) {
+								$('#message').html(response.message + "\n terlalu banyak upaya login"+ ", upaya login "+response.jumlah+"X  (lebih 5 kali upaya login, anda akan terkena Penalty)");
+								var secondd = response.penalty;
+							    display = $('#btn_login');
+							    startTimer(secondd, display); 
+								document.getElementById("btn_login").disabled = true;
+		                	}else if (response.jumlah>2 && response.jumlah<6){
+		                		$('#message').html(response.message + ", upaya login "+response.jumlah+"X  (lebih 5 kali upaya login, anda akan terkena Penalty)");
+		                	}else{
+		                		$('#message').html(response.message);
+		                	} 
+		                	//  menampilkan pesan errorr
 							$('#responseDiv').removeClass('alert-success').addClass('alert-danger').show(); 
 						}
 						else{
+							$('#message').html(response.message);
+
 							document.getElementById("loadbtn").style.display="inline-block";
+							document.getElementById("btn_login").disabled = true;
+
 							$('#responseDiv').removeClass('alert-danger').addClass('alert-success').show();
 							$('#uname').val("");
 							$('#paswd').val("");
@@ -44,9 +105,9 @@
 	                }
 	            });
 
-	            $(document).on('click', '#clearMsg',function(){
-					$('#responseDiv').hide();
-				});
+	   //          $(document).on('click', '#clearMsg',function(){
+				// 	$('#responseDiv').hide();
+				// });
 	            return false;
 	        });
 	       
